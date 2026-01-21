e43f3816d5588236385cf1ea25c15752ce9dbf9f
NITHISH <itsmenithish.official@gmail.com>
2026-01-21 17:18:57 +0530

    Initial commit: basic auth service

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const app = express();
+
+app.use(express.json());
+
+const MOCK_USER = {
+    email: "student@hostel.com",
+    password: "password123",
+    id: "user_555"
+};
+
+const SECRET_KEY = "my_super_secret_hostel_key";
+
+app.get('/', (req, res) => {
+    res.send('authGate is Online!');
+});
+
+
+app.post('/login' , (req,res) => {
+  const {email , password} = req.body;
+
+  if(email === MOCK_USER.email && password === MOCK_USER.password){
+    
+    const token = jwt.sign(
+      {userId: MOCK_USER.id , role: "student"},
+      SECRET_KEY,
+      {expiresIn:"1h"}
+    );
+
+    res.json({
+            message: "Login Successful!",
+            token: token
+        });
+    }
+
+    else{
+      res.status(401).json({message:"Invalid login"});
+    }
+
+});
+
+app.get('/dashboard' ,verifyToken  , (req,res) =>{
+    res.json({
+      message:`Welcome to the dashboard, User ${req.user.userId}`,
+      role: req.user.role  
+    });  
+});
+
+app.listen(3000, () => {
+    console.log(' authgate Server is running on 3000');
+});
+
